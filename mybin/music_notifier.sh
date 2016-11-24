dc6dc01c4a06269a8fb3f7ea5a033590f3b833b7
Kamel Hacene <kamel.h1206@gmail.com>
2016-11-24 15:38:00 +0100

    [enh] Map key to display the currently played music on cmus

--- mybin/music_notifier.sh
+++ mybin/music_notifier.sh
@@ -1,20 +1,50 @@
 #! /bin/sh
-CACHE_FILE=$HOME/.music_replace_id
+OPTIND=1 # Reset shell script invocation counter 
 
-if [ $# -ne 1 ] ; then
+CACHE_FILE=$HOME/.music_replace_id
+EXPORT_REPLACE_ID=$(cat $CACHE_FILE 2>/dev/null)
+if [[ -z $EXPORT_REPLACE_ID ]]; then
+    EXPORT_REPLACE_ID=0
+fi
+DEFAULT_DISPLAYED_TIME=2000
+
+print_help() {
+    echo "Usage : $0 [ARG]"
+    echo "Possible Arguments :"
+    echo "  -h  : print_help"
+    echo "  -c  : Use cmus-remote to get music name"
+    echo "  -t  : Displayed time in ms"
+    echo "  -p [String] : Print the string"
+}
+
+use_cmus=false
+display_string=""
+display_time=$DEFAULT_DISPLAYED_TIME
+
+while getopts "hcp:t:" OPT; do
+  case "$OPT" in
+    h) print_help;
+       exit;;
+    c) use_cmus=true;;
+    p) display_string=$OPTARG;;
+    t) display_time=$OPTARG;;
+    *) # getopts produces error
+       echo "Getopts error";
+       print_help;
+       exit;;
+  esac
+done
+
+if $use_cmus; then
+  display_string=$(cmus-remote -Q | sed -n -e 's/tag\s\(\w*\)/\1\t:\t/gp')
+fi
+
+if [ "$display_string" = "" ] ; then
     echo "No music name specified"
     exit 1
 fi
 
-EXPORT_REPLACE_ID=`cat $CACHE_FILE 2>/dev/null`
-
-if [[ -z $EXPORT_REPLACE_ID ]]; then
-    EXPORT_REPLACE_ID=0
-fi
-
-#echo Prev : $EXPORT_REPLACE_ID
-EXPORT_REPLACE_ID=`notify-send.sh -r $EXPORT_REPLACE_ID -t 3000 -i audio-volume-high "$1" -p`
-#echo Next : $EXPORT_REPLACE_ID
+EXPORT_REPLACE_ID=$(notify-send.sh -r $EXPORT_REPLACE_ID -t $display_time -i audio-volume-high "$display_string" -p)
 
 echo $EXPORT_REPLACE_ID > $CACHE_FILE
 
