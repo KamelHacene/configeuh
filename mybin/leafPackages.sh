8a1122c2be1af471ffe04d5b883cfa2cd5d7c3e6
Kamel Hacene <kamel.h1206@gmail.com>
2017-06-14 17:13:03 +0200

    [Script] Add a script that list installed and leaf packages. Update installed package list.

---
+++ mybin/leafPackages.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+installedPackages="$HOME/installedPackages.txt"
+leafPackages="$HOME/leafPackages.txt"
+
+pacman -Qq > $installedPackages
+
+> $leafPackages
+
+for package in $(cat $installedPackages); do
+  deps=$(($(pactree -r $package | wc -l) - 1))
+  if [ $deps -eq 0 ]; then
+    echo "$package" | tee $leafPackages
+  fi
+done
+
